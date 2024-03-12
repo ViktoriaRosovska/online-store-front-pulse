e9fba55ad08342fda858c0954438bb7217fec829
SvitlanaParyiska <paryiskasvitlana@gmail.com>
2024-03-12 23:45:54 +0200

    styled fixed and added mobile styles

--- src/layout/footer/copyright/copyright.jsx
+++ src/layout/footer/copyright/copyright.jsx
@@ -1,26 +1,15 @@
 import Text from "../../../components/text/text";
+import "./copyright.css";
 
 function Copyright() {
-    const styles = {
-        section: {
-            marginTop: "120px",
-            textAlign: "center"
-        },
-        text: {
-            color: "var(--white-text-color)",
-            fontSize: "14px"
-        }
-    }
-    
-    return (
-        <div className="copyright__section" style={styles.section}>
-            <Text
-                style={styles.text}
-                className="copyright__section-text" 
-                text="© 2024 PulseRun. Всі права захищені."
-            />
-        </div>
-    )
+  return (
+    <div className="copyright__section">
+      <Text
+        className="copyright__section-text"
+        text="© 2024 PulseRun. Всі права захищені."
+      />
+    </div>
+  );
 }
 
 export default Copyright;
