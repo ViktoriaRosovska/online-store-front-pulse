b78b71b98ed2195eaee2d69cb05823705e809cf7
Maksym Symonov <ssmaksss33@gmail.com>
2024-03-13 18:07:49 +0200

    update name

--- src/layout/menu/menu-item/menu-item.jsx
+++ src/layout/menu/menu-item/menu-item.jsx
@@ -2,7 +2,6 @@ import Links from "./../../../components/link/link";
 
 function MenuItem(props) {
 
-
     return (
         <li className={props.classNameLi}>
             <Links className={props.classNameLink} href={props.href} linkText={props.menuItemText}/>
