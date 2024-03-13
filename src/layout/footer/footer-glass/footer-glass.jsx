0665ae92a64b47c5e99b6dfe926a5ceb03c35f9c
SvitlanaParyiska <paryiskasvitlana@gmail.com>
2024-03-13 12:24:13 +0200

    stles fixed

--- src/layout/footer/footer-glass/footer-glass.jsx
+++ src/layout/footer/footer-glass/footer-glass.jsx
@@ -1,7 +1,6 @@
 import logoFooterImg from "/logo-footer.svg";
 import FooterGlassInfo from "./footer-glass-info/footer-glass-info";
 import Copyright from "../copyright/copyright";
-import MediaQuery from "react-responsive";
 import "./footer-glass.css";
 
 function FooterGlass() {
@@ -9,16 +8,9 @@ function FooterGlass() {
     <div className="footer__glass">
       <div className="container">
         <div className="footer__glas-inner">
-          <MediaQuery maxWidth={1439}>
-            <div className="footer__glass-logo">
-              <img src={logoFooterImg} alt="" />
-            </div>
-          </MediaQuery>
-          <MediaQuery minWidth={1440}>
-            <div className="footer__glass-logo">
-              <img src={logoFooterImg} alt="" />
-            </div>
-          </MediaQuery>
+          <div className="footer__glass-logo">
+            <img src={logoFooterImg} alt="" />
+          </div>
           <FooterGlassInfo />
           <Copyright />
         </div>
