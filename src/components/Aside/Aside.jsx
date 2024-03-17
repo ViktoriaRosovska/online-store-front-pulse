c8f9332aad5c15c471b8178a46f10716f03c1682
Viktoria Rosovska <torimeister@gmail.com>
2024-03-17 23:45:21 +0200

    work with checkbox

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -2,15 +2,21 @@ import { CheckboxList } from "../CheckboxList/CheckboxList";
 import brandList from "../../data/brendList.json";
 import seasonList from "../../data/seasonList.json";
 import sizeList from "../../data/sizeList.json";
+import colorList from "../../data/colorList.json";
 import { AsideList } from "./Aside.styled";
 
 export const Aside = (props) => {
-  console.log(brandList);
   return (
     <AsideList>
-      <CheckboxList items={brandList} title="Брeнд" onChanged={(items) => props.onChanged("brand", items)} />
+      <CheckboxList
+        items={brandList}
+        title="Брeнд"
+        checked={props.selectedBrands}
+        onChanged={(items) => props.onChanged("brand", items)}
+      />
       <CheckboxList items={seasonList} title="Сезон" onChanged={(items) => props.onChanged("season", items)} />
       <CheckboxList items={sizeList} title="Розмір" onChanged={(items) => props.onChanged("size", items)} />
+      <CheckboxList items={colorList} title="Колір" onChanged={(items) => props.onChanged("color", items)} />
     </AsideList>
   );
 };
--- src/components/CatalogHeader/CatalogHeader.styled.js
+++ src/components/CatalogHeader/CatalogHeader.styled.js
@@ -1,15 +1,18 @@
 import styled from "styled-components";
 
 const CatalogHeaderContainer = styled.div`
-  display: flex;
+  /* display: flex;
   justify-content: space-between;
-  align-items: center;
+  align-items: center;*/
   margin-bottom: 24px;
 `;
 const FilterWrapper = styled.div`
   display: flex;
   gap: 17px;
-  margin-bottom: 24px;
+  width: fit-content;
+
+  /* margin-bottom: 24px; */
+  /* border: 1px solid red; */
 `;
 const FilterWrapperButton = styled.button`
   border: none;
@@ -20,5 +23,16 @@ const FilterWrapperButton = styled.button`
   line-height: 14px;
   letter-spacing: 0.01px;
   padding: 2.5px 9px;
+  min-width: 125px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+  stroke: var(--black-text-color);
+  &:hover {
+    color: var(--white-text-color);
+    stroke: var(--white-text-color);
+    background-color: var(--black-bg-color);
+  }
 `;
 export { CatalogHeaderContainer, FilterWrapper, FilterWrapperButton };
