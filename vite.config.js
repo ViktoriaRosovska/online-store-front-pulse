e89d5d16fe4f052820f87ad1b93e146e62beb151
Viktoria Rosovska <torimeister@gmail.com>
2024-03-29 00:38:26 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,18 @@
-import { defineConfig, transformWithEsbuild } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 import svgr from "@svgr/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    {
-      name: "treat-js-files-as-jsx",
-      async transform(code, id) {
-        if (!id.match(/src\/.*\.js$/)) return null;
+  plugins: [[react()], [svgr()]],
 
-        // Use the exposed transform from vite, instead of directly
-        // transforming with esbuild
-        return transformWithEsbuild(code, id, {
-          loader: "jsx",
-          jsx: "automatic",
-        });
-      },
-    },
-
-    react(),
-    svgr(),
-  ],
-
-  optimizeDeps: {
-    force: true,
-    esbuildOptions: {
-      loader: {
-        ".js": "jsx",
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      src: "/src",
-      components: "/src/components",
-    },
-  },
-
+  // resolve: {
+  //   alias: {
+  //     src: "/src",
+  //     components: "/src/components",
+  //   },
+  // },
+  extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   base: "/online-store-front-pulse/",
 });
