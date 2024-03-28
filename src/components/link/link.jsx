0890b4fd61145d9dcbb15ce4256a52f042201932
Viktoria Rosovska <torimeister@gmail.com>
2024-03-28 12:25:47 +0200

    update

--- src/components/link/link.jsx
+++ src/components/link/link.jsx
@@ -1,9 +1,11 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Links(props) {
-    return (
-        <Link className={props.className} to={props.href}>{props.linkText || props.children}</Link>
-    )
+  return (
+    <Link className={props.className} to={props.href}>
+      {props.linkText || props.children}
+    </Link>
+  );
 }
 
 export default Links;
