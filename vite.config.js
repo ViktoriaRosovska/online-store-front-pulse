0a7458f995613aa58f0544060c3ce6203efc987c
Viktoria Rosovska <torimeister@gmail.com>
2024-03-29 00:30:21 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,43 @@
-import { defineConfig } from "vite";
+import { defineConfig, transformWithEsbuild } from "vite";
 import react from "@vitejs/plugin-react";
 
 import svgr from "@svgr/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [[react()], [svgr()]],
-  // resolve: {
-  //   alias: {
-  //     src: "/src",
-  //     components: "/src/components",
-  //   },
-  // },
+  plugins: [
+    {
+      name: "treat-js-files-as-jsx",
+      async transform(code, id) {
+        if (!id.match(/src\/.*\.js$/)) return null;
+
+        // Use the exposed transform from vite, instead of directly
+        // transforming with esbuild
+        return transformWithEsbuild(code, id, {
+          loader: "jsx",
+          jsx: "automatic",
+        });
+      },
+    },
+
+    react(),
+    svgr(),
+  ],
+
+  optimizeDeps: {
+    force: true,
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+    },
+  },
 
   base: "/online-store-front-pulse/",
 });
