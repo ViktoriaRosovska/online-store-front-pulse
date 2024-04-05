c9779850fa26cf9cc6bf5f25272851f3f3de5a4b
Anton <a.chertok@akatech.com.ua>
2024-04-05 16:10:02 +0100

    Update main.styled.js

--- src/main.styled.js
+++ src/main.styled.js
@@ -14,24 +14,13 @@ const Container = styled.div`
   width: 100%;
   margin: 0 auto;
   padding: 0 10px;
-  /* border: 1px solid red; */
-
-  /* @media screen and (min-width: 375px) and (max-width: 768px) {
-    width: 375px;
-    padding: 0 24px;
-  } */
 
   @media screen and (min-width: 375px) {
     width: 375px;
     padding: 0 24px;
   }
-  /* @media screen and (min-width: 1440px) {
-    width: 1440px;
-    padding: 0 60px;
-  } */
 
   @media screen and (min-width: 1440px) {
-    /* min-width: 1440px; */
     width: 1440px;
     padding: 0 120px;
   }
