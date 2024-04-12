2acc946bf7b500451e63d37404b9bd6778cc6cba
Anton <a.chertok@akatech.com.ua>
2024-04-12 23:28:46 +0100

    feat(): location link button

--- src/components/CatalogComponent/CatalogNavigation/CatalogNavigation.jsx
+++ src/components/CatalogComponent/CatalogNavigation/CatalogNavigation.jsx
@@ -1,9 +1,11 @@
+import Breadcrumbs from "components/Breadcrumbs";
 import { CatalogNavigationWrapper } from "./CatalogNavigation.styled";
 
 export const CatalogNavigation = props => {
   return (
     <CatalogNavigationWrapper>
-      <a href="./">Головна</a> <span>/</span> <span>{props.title}</span>
+      {/* <a href="./">Головна</a> <span>/</span> <span>{props.title}</span> */}
+      <Breadcrumbs current={props.title} />
     </CatalogNavigationWrapper>
   );
 };
