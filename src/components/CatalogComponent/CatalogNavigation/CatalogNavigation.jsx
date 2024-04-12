e46e41f5b31b6ce8179e50165b431003ca46906d
Anton <a.chertok@akatech.com.ua>
2024-04-12 23:11:35 +0100

    Update CatalogNavigation.jsx

--- src/components/CatalogComponent/CatalogNavigation/CatalogNavigation.jsx
+++ src/components/CatalogComponent/CatalogNavigation/CatalogNavigation.jsx
@@ -1,11 +1,9 @@
-import Breadcrumbs from "components/Breadcrumbs";
 import { CatalogNavigationWrapper } from "./CatalogNavigation.styled";
 
 export const CatalogNavigation = props => {
   return (
     <CatalogNavigationWrapper>
-      {/* <a href="./">Головна</a> <span>/</span> <span>{props.title}</span> */}
-      <Breadcrumbs current={props.title} />
+      <a href="./">Головна</a> <span>/</span> <span>{props.title}</span>
     </CatalogNavigationWrapper>
   );
 };
