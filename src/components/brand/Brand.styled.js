d11437da147accf016afc1cb232229ce404405ec
Viktoria Rosovska <torimeister@gmail.com>
2024-04-28 03:40:52 +0300

    work with brand slider

--- src/components/brand/Brand.styled.js
+++ src/components/brand/Brand.styled.js
@@ -1,9 +1,8 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 
-export const Container=styled.div`
-padding:120px 120px;
-`
+export const Container = styled.div`
+  padding: 120px 120px;
+`;
 export const NavigationWrapper = styled.div`
   font-size: 16px;
   line-height: 20px;
@@ -14,29 +13,60 @@ export const NavigationWrapper = styled.div`
     color: var(--grey-text-color);
   }
 `;
-export const Title= styled.h1`
-font-family: var(--tittle-font);
-font-weight: 400;
-font-size: 36px;
-line-height: 100%;
-letter-spacing: 0;
-margin-bottom:36px;
-text-align:center;`
+export const Title = styled.h1`
+  font-family: var(--tittle-font);
+  font-weight: 400;
+  font-size: 36px;
+  line-height: 100%;
+  letter-spacing: 0;
+  margin-bottom: 36px;
+  text-align: center;
+`;
 
-export const Text= styled.p`
-font-family: "Roboto";
-    font-size: 16px;
-    line-height: 20px;
-    font-weight: 400;
-    color: var(--black-text-color);
-    margin-top: 12px;
-    text-align:center;
-    `
-    export const Links= styled(Link)`
-display:flex;
-flex-direction:column;
-align-items:center;
-justify-content:center:
-    `
+export const Text = styled.p`
+  font-family: "Roboto";
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: 400;
+  color: var(--black-text-color);
+  text-align: center;
+`;
+// export const Links = styled(Link)``;
 
+const StyledBrandCardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 152px;
+  height: 133px;
+  @media screen and (min-width: 1440px) {
+    width: 170px;
+    height: 147px;
+  }
+`;
 
+const StyledBrandImage = styled.img`
+  width: 152px;
+  height: fit-content;
+  @media screen and (min-width: 1440px) {
+    width: 170px;
+  }
+`;
+
+const SliderBrandsWrapper = styled.div`
+  position: relative;
+  /* display: flex; */
+  /* border: 1px solid red; */
+  /* flex-direction: row; */
+  /* flex-wrap: wrap; */
+  /* width: 327px; */
+
+  height: 550px;
+
+  @media screen and (min-width: 1440px) {
+    height: 147px;
+    /* width: 1198px; */
+  }
+`;
+export { StyledBrandCardWrapper, StyledBrandImage, SliderBrandsWrapper };
--- src/components/brand/brand.jsx
+++ src/components/brand/brand.jsx
@@ -1,16 +1,28 @@
 import { useState, useEffect } from "react";
 
+import "swiper/css/grid";
 import {
-  Container,
+  // Container,
   NavigationWrapper,
   Title,
   Text,
-  Links,
+  StyledBrandCardWrapper,
+  StyledBrandImage,
+  SliderBrandsWrapper,
 } from "./Brand.styled";
+
+import "./brandSlider.css";
 import { brands } from "./index";
-
-import "./swiper.module.css";
-
+import {
+  StyledNavigationNextBtn,
+  StyledNavigationPrevBtn,
+} from "../Slider/ProductSlider.styled.js";
+import { ReactComponent as SwiperRightArrowIcon } from "../../assets/svg/swiperRightArrow.svg";
+import { ReactComponent as SwiperLeftArrowIcon } from "../../assets/svg/swiperLeftArrow.svg";
+import { Link } from "react-router-dom";
+import { Container, PageSection } from "../../main.styled";
+import { register } from "swiper/element";
+register();
 export default function BrandsList(props) {
   const [slidesPerView, setSlidesPerView] = useState(5);
 
@@ -53,44 +65,88 @@ export default function BrandsList(props) {
   }, []);
 
   return (
-    <Container>
-      <NavigationWrapper>
-        <a href="./">Головна</a> <span>/</span> <span>{props.title}</span>
-      </NavigationWrapper>
-      <Title>Бренди</Title>
-      <div className="swiper ">
-        <swiper-container
-          spaceBetween={10}
-          navigation={true}
-          slides-per-view={slidesPerView}
-          loading="lazy"
-          style={{
-            "--swiper-button-prev-right": "auto",
-            "--swiper-button-next-background-image":
-              "url(/icons/White Circular Buttons.png)",
-            "--swiper-navigation-color": "black",
-          }}
-          scrollbar="false"
-        >
-          {brands["id"].map(index => {
-            return (
-              <swiper-slide class="swiper" key={index}>
-                <Links to={`/catalog?brand=${brands["title"][index - 1]}`}>
-                  <img
-                    src={brands["img"][index - 1]}
-                    alt={brands["title"][index - 1]}
-                    width={149}
-                    height={101}
-                    loading="eager"
-                  />
-                  <Text>{brands["title"][index - 1]}</Text>
-                </Links>
-              </swiper-slide>
-            );
-          })}
-          ...
-        </swiper-container>
-      </div>
-    </Container>
+    <PageSection>
+      <Container>
+        <NavigationWrapper>
+          <a href="./">Головна</a> <span>/</span> <span>{props.title}</span>
+        </NavigationWrapper>
+        <Title>Бренди</Title>
+        <SliderBrandsWrapper>
+          <StyledNavigationPrevBtn className="nav-btn custom-prev-button">
+            <SwiperLeftArrowIcon />
+          </StyledNavigationPrevBtn>
+
+          <StyledNavigationNextBtn className="nav-btn custom-next-button">
+            <SwiperRightArrowIcon />
+          </StyledNavigationNextBtn>
+          <swiper-container
+            className="swiper-wrapper"
+            spaceBetween={10}
+            navigation={true}
+            loop={true}
+            // slides-per-view={slidesPerView}
+            loading="lazy"
+            // pagination={{
+            //   clickable: true,
+            //   bulletClass: `swiper-pagination`,
+            //   el: ".swiper-pagination",
+            // }}
+            navigation-next-el=".custom-next-button"
+            navigation-prev-el=".custom-prev-button"
+            breakpoints={JSON.stringify({
+              320: {
+                slidesPerView: 2,
+
+                grid: {
+                  rows: 3,
+                  column: 2,
+                  fill: "row",
+                },
+
+                spaceBetween: 23,
+                slidesPerGroup: 2,
+                // vertical: true,
+              },
+
+              1440: {
+                slidesPerView: 5,
+                spaceBetween: 87,
+              },
+            })}
+            style={{
+              "--swiper-button-prev-right": "auto",
+              "--swiper-navigation-color": "black",
+            }}
+            scrollbar="false"
+          >
+            {brands && brands.length > 0 ? (
+              brands.map(el => {
+                return (
+                  <swiper-slide class="swiper-slide" key={el.title}>
+                    <Link
+                      to={`/catalog?brand=${el.title}`}
+                      style={{ width: "170px" }}
+                    >
+                      <StyledBrandCardWrapper>
+                        <StyledBrandImage
+                          src={el.img}
+                          alt={el.title}
+                          // width={149}
+                          // height={101}
+                          loading="eager"
+                        />
+                        <Text>{el.title}</Text>
+                      </StyledBrandCardWrapper>
+                    </Link>
+                  </swiper-slide>
+                );
+              })
+            ) : (
+              <div>Loading...</div>
+            )}
+          </swiper-container>
+        </SliderBrandsWrapper>
+      </Container>
+    </PageSection>
   );
 }
