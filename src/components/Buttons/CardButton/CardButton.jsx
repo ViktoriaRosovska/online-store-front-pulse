2aa032fb42f015070f8e12e5446749cc10d7d675
Viktoria Rosovska <torimeister@gmail.com>
2024-04-30 01:29:49 +0300

    fix brands

--- src/components/Buttons/CardButton/CardButton.jsx
+++ src/components/Buttons/CardButton/CardButton.jsx
@@ -1,11 +1,16 @@
 // import { useLocation } from "react-router-dom";
 import { CardButtonWrapper } from "./CardButton.styled";
 
-const CardButton = ({ text, style, route, state }) => {
+const CardButton = ({ text, style, route, state, $bannerWidth }) => {
   // const location = useLocation().pathname;
 
   return (
-    <CardButtonWrapper style={style} to={route} state={state}>
+    <CardButtonWrapper
+      style={style}
+      to={route}
+      state={state}
+      $bannerWidth={$bannerWidth}
+    >
       {text}
     </CardButtonWrapper>
   );
