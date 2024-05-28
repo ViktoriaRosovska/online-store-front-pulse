bfc180c9aeaebec4f3d9700ecbade1e3dad3f66d
Janne57 <evgeniyahome1981@gmail.com>
2024-05-28 20:00:38 +0300

    fix footer's page About Us

--- src/pages/footer/FooterAboutUs.jsx
+++ src/pages/footer/FooterAboutUs.jsx
@@ -1,13 +1,14 @@
 import MediaQuery from "react-responsive";
 import photoFounder from "../../assets/images/photoAboutUs.jpg";
 import photoFounderMob from "../../assets/images/photoAboutUsMobile.jpg";
+import DetailsTogglerSecond from "components/UIKit/DetailsTogglerSecond/DetailsTogglerSecond";
 import { Container, PageSection } from "../../main.styled";
 import {
   FooterPages,
   FooterItemText,
+  FooterItemBottom,
   FooterSection,
   FooterSectionColumn,
-  FooterSectionAboutUs,
 } from "./FooterPages.styled";
 
 const FooterAboutUs = () => {
@@ -17,27 +18,43 @@ const FooterAboutUs = () => {
         <FooterPages>
           <MediaQuery maxWidth={1439.98}>
             <h3>Про нас</h3>
-            <FooterSectionAboutUs>
+            <FooterSectionColumn>
               <img
                 src={photoFounderMob}
                 alt="Photo portrait of the founder"
                 width="328"
                 height="327"
               />
-
               <FooterItemText>
                 Ласкаво просимо до RunPulse – мого улюбленого куточку для тих,
                 хто розуміє важливість комфорту та стилю у спортивному взутті!
               </FooterItemText>
               <FooterItemText>
-                Мене звати Оксана, і я є засновницею цього магазину. Як жінка та
-                поціновувачка активного способу життя, я розумію, наскільки
-                важливо мати вірного спутника на кожному кроці свого шляху. Сама
-                знаю, як важливо мати взуття, яке не тільки підтримує, але і
-                надихає до досягнень.
+                <FooterItemBottom>
+                  Мене звати Оксана, і я є засновницею цього магазину. Як жінка
+                  та поціновувачка активного способу життя, я розумію, наскільки
+                  важливо мати вірного спутника на кожному кроці свого шляху.
+                  Сама знаю, як важливо мати взуття, яке не тільки підтримує,
+                  але і надихає до досягнень.
+                </FooterItemBottom>
               </FooterItemText>
-              <a>Розгорнути</a>
-            </FooterSectionAboutUs>
+
+              <DetailsTogglerSecond>
+                <FooterItemText>
+                  "У RunPulse ми пропонуємо вам великий вибір брендового та
+                  класичного спортивного взуття. Ми віримо, що правильний вибір
+                  взуття може зробити ваші тренування більш ефективними та
+                  приємними. Кожна пара, яку ми обираємо для нашого магазину,
+                  проходить відбір за якістю та стилем, щоб ви могли бути
+                  впевнені в своєму виборі. Моя команда та я прагнемо зробити
+                  ваші покупки максимально приємними та зручними. Будьте
+                  впевнені, що кожна пара взуття від RunPulse призначена для
+                  того, щоб допомагати вам досягати нових висот у спорті. Дякую,
+                  що обрали RunPulse – ваш партнер у кожному спортивному
+                  досягненні!"
+                </FooterItemText>
+              </DetailsTogglerSecond>
+            </FooterSectionColumn>
           </MediaQuery>
 
           <MediaQuery minWidth={1440}>
@@ -55,13 +72,30 @@ const FooterAboutUs = () => {
                   хто розуміє важливість комфорту та стилю у спортивному взутті!
                 </FooterItemText>
                 <FooterItemText>
-                  Мене звати Оксана, і я є засновницею цього магазину. Як жінка
-                  та поціновувачка активного способу життя, я розумію, наскільки
-                  важливо мати вірного спутника на кожному кроці свого шляху.
-                  Сама знаю, як важливо мати взуття, яке не тільки підтримує,
-                  але і надихає до досягнень.
+                  <FooterItemBottom>
+                    Мене звати Оксана, і я є засновницею цього магазину. Як
+                    жінка та поціновувачка активного способу життя, я розумію,
+                    наскільки важливо мати вірного спутника на кожному кроці
+                    свого шляху. Сама знаю, як важливо мати взуття, яке не
+                    тільки підтримує, але і надихає до досягнень.
+                  </FooterItemBottom>
                 </FooterItemText>
-                <a>Розгорнути</a>
+
+                <DetailsTogglerSecond>
+                  <FooterItemText>
+                    "У RunPulse ми пропонуємо вам великий вибір брендового та
+                    класичного спортивного взуття. Ми віримо, що правильний
+                    вибір взуття може зробити ваші тренування більш ефективними
+                    та приємними. Кожна пара, яку ми обираємо для нашого
+                    магазину, проходить відбір за якістю та стилем, щоб ви могли
+                    бути впевнені в своєму виборі. Моя команда та я прагнемо
+                    зробити ваші покупки максимально приємними та зручними.
+                    Будьте впевнені, що кожна пара взуття від RunPulse
+                    призначена для того, щоб допомагати вам досягати нових висот
+                    у спорті. Дякую, що обрали RunPulse – ваш партнер у кожному
+                    спортивному досягненні!"
+                  </FooterItemText>
+                </DetailsTogglerSecond>
               </FooterSectionColumn>
             </FooterSection>
           </MediaQuery>
