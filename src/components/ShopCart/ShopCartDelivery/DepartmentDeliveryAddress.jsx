e1d078c3b7243a6db17ca3e43dbed5f568b665be
Viktoria Rosovska <torimeister@gmail.com>
2024-06-21 10:21:46 +0300

    Improved Department selector

--- src/components/ShopCart/ShopCartDelivery/DepartmentDeliveryAddress.jsx
+++ src/components/ShopCart/ShopCartDelivery/DepartmentDeliveryAddress.jsx
@@ -9,28 +9,35 @@ import {
 } from "./ShopCartDelivery.styled";
 import { addShopCartAddress } from "../../../redux/user/userShopCart/userShopCartSlice";
 import { selectUserShopCart } from "../../../redux/user/userShopCart/userShopCartSelector";
+import { useEffect, useState } from "react";
 
 export const DepartmentDeliveryAddress = () => {
-  const { city } = useSelector(selectUserShopCart);
+  const { city, address } = useSelector(selectUserShopCart);
   const dispatch = useDispatch();
   const [
     getDepartments,
     {
-      data: departmentData,
+      data,
       // isError: departmentIsError,
       // isLoading: departmentIsLoading,
     },
   ] = useGetDepartmentsMutation();
 
+  const [search, setSearch] = useState(address.Description);
+  useEffect(() => {
+    getDepartments(city.Ref, search);
+  }, [city, search, getDepartments]);
+
   const onSelectDepartmentSearch = (ref, value) => {
     console.log("onSelectDepartmentSearch", ref, value);
-    getDepartments(ref, value);
+    if (value !== "") setSearch(value);
   };
 
   const onSelectDepartmentsChange = value => {
     console.log("onSelectDepartmentsChange", value);
-    dispatch(addShopCartAddress(value.label));
+    dispatch(addShopCartAddress(value));
   };
+
   return (
     <>
       <StyledDeliveryTitle>Адреса відділення</StyledDeliveryTitle>
@@ -40,12 +47,11 @@ export const DepartmentDeliveryAddress = () => {
           Оберіть номер відділення
         </StyledSelectLabel>
         <DepartmentSelect
-          options={departmentTypeFilter(departmentData, ["Branch", "Store"])}
+          options={departmentTypeFilter(data, ["Branch", "Store"])}
           placeholder="Номер відділення"
-          onChange={e => {
-            onSelectDepartmentsChange(e);
-          }}
+          onChange={e => onSelectDepartmentsChange(e)}
           onSearch={e => onSelectDepartmentSearch(city.Ref, e)}
+          displayDepartment={address}
           name="address"
         />
       </StyledSelectWrapper>
