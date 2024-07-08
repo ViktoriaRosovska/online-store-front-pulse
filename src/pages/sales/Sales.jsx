b66c158cf2b2a7e44a560b396a6c2a9a769c7379
SvetlanaBuuvol <divinesvetok@gmail.com>
2024-07-08 22:44:59 +0300

    default export catalogComponent

--- src/pages/sales/Sales.jsx
+++ src/pages/sales/Sales.jsx
@@ -1,5 +1,5 @@
 import { Helmet } from "react-helmet";
-import { CatalogComponent } from "../../components/CatalogComponent/CatalogComponent";
+import CatalogComponent from "../../components/CatalogComponent/CatalogComponent";
 import { useLazyGetSalesQuery } from "../../redux/products/productsApi";
 
 const Sales = () => {
