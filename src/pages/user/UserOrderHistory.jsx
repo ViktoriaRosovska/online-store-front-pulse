b49de62caa661dbb242142340af6d752232216bf
Viktoria Rosovska <torimeister@gmail.com>
2024-08-15 18:14:05 +0300

    buyHistory sort by last

--- src/pages/user/UserOrderHistory.jsx
+++ src/pages/user/UserOrderHistory.jsx
@@ -5,10 +5,20 @@ import { useGetUserOrdersQuery } from "../../redux/auth/userAuthApi";
 const UserOrderHistory = () => {
   const { data } = useGetUserOrdersQuery();
 
+  let newOrder = [];
+  if (data?.buyHistory) {
+    newOrder = [...data.buyHistory];
+  }
+  newOrder.sort(function (a, b) {
+    const c = new Date(a.orderDate);
+    const d = new Date(b.orderDate);
+    return d - c;
+  });
+
   return (
     <>
       <Title style={{ textAlign: "left" }}>Історія замовлень</Title>
-      <UserOrderHistoryList orders={data?.buyHistory} />
+      <UserOrderHistoryList orders={newOrder} />
     </>
   );
 };
