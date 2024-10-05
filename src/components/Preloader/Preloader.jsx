9949944371d1f2e55f7e4335b8c58234e44f3eed
Viktoria Rosovska <torimeister@gmail.com>
2024-10-05 21:11:04 +0300

    add preloader

---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,5 @@
+import { StyledPreloadWrapper } from "./Preloader.styled";
+
+export const Preloader = () => {
+  return <StyledPreloadWrapper>Pulse Run</StyledPreloadWrapper>;
+};
---
+++ src/components/Preloader/Preloader.styled.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const StyledPreloadWrapper = styled.div`
+  background-color: black;
+  width: 100vw;
+  height: 100vh;
+  color: white;
+  font-size: 200px;
+  font-family: "Roboto";
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export { StyledPreloadWrapper };
